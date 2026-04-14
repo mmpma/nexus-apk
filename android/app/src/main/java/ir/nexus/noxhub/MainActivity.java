22ddca743419d77735c508d70fc2c725520633cd
mmpma <69171083+mmpma@users.noreply.github.com>
2026-04-15 01:45:05 +0330

    Create MainActivity.java

---
+++ android/app/src/main/java/ir/nexus/noxhub/MainActivity.java
@@ -0,0 +1,14 @@
+package ir.nexus.noxhub;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // قفل کردن اسکرین‌شات و ضبط صفحه
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    }
+}
